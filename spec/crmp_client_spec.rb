23f9eb8711ea2a23210def106c095f0fb9884b2d
asibs <andrew.s@38degrees.org.uk>
2021-01-26 22:25:28 +0000

    Add CrmpClient configuration tests

--- spec/crmp_client_spec.rb
+++ spec/crmp_client_spec.rb
@@ -5,7 +5,50 @@ RSpec.describe CrmpClient do
     expect(CrmpClient::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context '.new' do
+    let(:default_base_uri) { 'https://crmp.default.org' }
+    let(:default_api_token) { 'strong_token' }
+
+    let(:override_base_uri) { 'https://crmp.non-standard.org' }
+    let(:override_api_token) { 'another_strong_token' }
+
+    context 'when CrmpClient has not been configured' do
+      context 'when params are not provided' do
+        it 'attempts to constructs a Client with nil arguments' do
+          expect(CrmpClient::Client).to receive(:new).with(nil, nil)
+          CrmpClient.new
+        end
+      end
+
+      context 'when params are provided' do
+        it 'constructs a Client with the provided params' do
+          expect(CrmpClient::Client).to receive(:new).with(override_base_uri, override_api_token)
+          CrmpClient.new(override_base_uri, override_api_token)
+        end
+      end
+    end
+
+    context 'when CrmpClient has been configured' do
+      before do
+        CrmpClient.configure do |config|
+          config.default_base_uri = default_base_uri
+          config.default_api_token = default_api_token
+        end
+      end
+
+      context 'when params are not provided' do
+        it 'constructs a Client with default arguments' do
+          expect(CrmpClient::Client).to receive(:new).with(default_base_uri, default_api_token)
+          CrmpClient.new
+        end
+      end
+
+      context 'when params are provided' do
+        it 'constructs a Client with the provided params' do
+          expect(CrmpClient::Client).to receive(:new).with(override_base_uri, override_api_token)
+          CrmpClient.new(override_base_uri, override_api_token)
+        end
+      end
+    end
   end
 end
